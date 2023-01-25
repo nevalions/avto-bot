448cddd166650457be92f1c8a7219bbd3b2d97e2
nevalions <nevalions@ya.ru>
2023-01-25 12:30:09 +0300

    Refactor db_helper.py

--- src/db_helper.py
+++ src/db_helper.py
@@ -6,7 +6,6 @@ import db_main_queries as queries
 
 
 class AutoBotDB:
-    users_db_table_name = 'users'
     cars_db_table_name = 'cars'
 
     def __init__(self, host_db=host, user_db=user, pass_db=password, db=db_name):
@@ -26,64 +25,6 @@ class AutoBotDB:
     def query_execute(self, query):
         self.cursor.execute(query)
         self.connect.commit()
-
-    def add_user(self, _username, _email):
-        try:
-            query = sql.SQL("INSERT INTO users(username, email) SELECT {username}, {email} WHERE NOT EXISTS (SELECT "
-                            "id FROM users WHERE email = {email}) RETURNING id").format(username=sql.Literal(_username),
-                                                                                        email=sql.Literal(_email))
-
-            self.cursor.execute(query)
-            self.connect.commit()
-            user_id = self.cursor.fetchone()[0]
-            print(f'{_username} with ID: {user_id} added to DB')
-            return user_id
-        except Exception as ex:
-            print(ex)
-            print(f'Error adding user to DB, user with email {_email} already exist')
-
-    def update_user_username(self, u_email, new_name):
-        try:
-            key_name = 'email'
-            key = u_email
-            value_name = 'username'
-            new_value = new_name
-
-            query = queries.update_str_value_in_db_by_key(
-                self.users_db_table_name,
-                key_name,
-                key,
-                value_name,
-                new_value
-            )
-            self.cursor.execute(query)
-            self.connect.commit()
-            if self.cursor.fetchone():
-                print(f'User {new_name} with email: {u_email} updated')
-            else:
-                raise TypeError(f'User with email {u_email} not exist exist')
-        except Exception as ex:
-            print(ex)
-            print(f'Error updating user in DB')
-
-    def get_all_users_in_db(self):
-        self.cursor.execute(queries.get_all_rows_from_db(self.users_db_table_name))
-        all_users = self.cursor.fetchall()
-        if all_users:
-            return all_users
-        else:
-            print('No users in DB')
-            raise Exception
-
-    def get_user_by_user_id(self, user_id: int):
-        query = queries.get_db_item_by_id(self.users_db_table_name, user_id)
-        self.cursor.execute(query)
-        car = self.cursor.fetchall()
-        if car:
-            return car[0]
-        else:
-            print(f'No users with ID {user_id}')
-            raise Exception
 
     def add_car(self, model: str, model_name: str, mileage: str, measures: str, date_added: str, description: str):
         try:
