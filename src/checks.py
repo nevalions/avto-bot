875cd54728f998cc91cfd0f6ea364c5c7df3032c
nevalions <nevalions@ya.ru>
2023-01-28 16:15:27 +0300

    logging added

--- src/checks.py
+++ src/checks.py
@@ -1,7 +1,16 @@
+import logging.config
 from email_validator import validate_email, EmailNotValidError
+
+from log_dir.log_conf import LOGGING_CONFIG
+from log_dir.func_auto_log import autolog_debug
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 
 def email_is_valid(mail: str):
+    autolog_debug(f'Check email {mail}')
     m = mail.strip()
     if m != validate_email(m).email:
         raise EmailNotValidError()
+
