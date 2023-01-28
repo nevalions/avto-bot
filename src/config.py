811847814947efdf4fb557d40c08dca684ade1c2
nevalions <nevalions@ya.ru>
2023-01-28 16:16:57 +0300

    root_dir and src_dir addded to config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,9 @@
+import pathlib
+
+SRC_DIR = pathlib.Path('')
+SRC_DIR_ABSOLUTE = pathlib.Path.absolute(SRC_DIR)
+ROOT_DIR = pathlib.Path.absolute(SRC_DIR).parents[0]
+
 host = '127.0.0.1'
 user = 'avto'
 password = 'kicker'
