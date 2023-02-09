bf70f94581f01700f2c9fe918e030d3378332991
nevalions <linroot@bk.ru>
2023-02-09 14:52:37 +0300

    3.11 added

--- src/tg_bot/app.py
+++ src/tg_bot/app.py
@@ -3,6 +3,7 @@ async def on_startup(_dp):
     # from utils.notify_admins import on_startup_notify
     # await on_startup_notify(_dp)
 
+
     from utils.set_bot_commands import set_default_commands
     await set_default_commands(_dp)
 
