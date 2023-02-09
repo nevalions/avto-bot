408638cef1325535719edc18d3f8cf0146b28dfa
nevalions <linroot@bk.ru>
2023-02-10 00:14:03 +0300

    edit log

--- src/cars.py
+++ src/cars.py
@@ -1,8 +1,10 @@
 import logging.config
+
 import string
 from dateutil.parser import parse
 
 from log_dir.log_conf import LOGGING_CONFIG
+
 from log_dir.func_auto_log import autolog_info, autolog_warning
 
 logging.config.dictConfig(LOGGING_CONFIG)
