f5e6a26441b694008e82bbc63690eb9eefd7a727
nevalions <linroot@bk.ru>
2023-02-09 14:53:22 +0300

    minor

--- src/tg_bot/app.py
+++ src/tg_bot/app.py
@@ -3,7 +3,6 @@ async def on_startup(_dp):
     # from utils.notify_admins import on_startup_notify
     # await on_startup_notify(_dp)
 
-
     from utils.set_bot_commands import set_default_commands
     await set_default_commands(_dp)
 
