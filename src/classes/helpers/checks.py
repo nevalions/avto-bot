2da0da95f8118f17c14367a45650c1b44a99ee6d
nevalions <linroot@bk.ru>
2023-02-15 21:43:20 +0300

    project refactoring testing

--- src/classes/helpers/checks.py
+++ src/classes/helpers/checks.py
@@ -4,10 +4,9 @@ import os
 from email_validator import validate_email, EmailNotValidError
 import logging.config
 
-sys.path.append(os.path.join(os.getcwd(), '..'))
-sys.path.append(os.path.join(os.getcwd(), '..'))
-from src.log_dir.log_conf import LOGGING_CONFIG
-from src.log_dir.func_auto_log import autolog_debug
+sys.path.append(os.path.join(os.getcwd(), '../..'))
+from log_dir.log_conf import LOGGING_CONFIG
+from log_dir.func_auto_log import autolog_debug
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
