27f207a0532bfe230f6fc69c69eadab02fd6ca33
nevalions <linroot@bk.ru>
2023-02-15 18:26:29 +0300

    project refactoring

--- src/log_dir/__init__.py
+++ src/log_dir/__init__.py
@@ -1,2 +1,7 @@
-# from . func_auto_log import *
-# from . log_conf import *
+import sys
+import os
+
+# sys.path.append(os.path.join(os.getcwd(), 'log_dir'))
+
+# from func_auto_log import autolog_warning, autolog_info, autolog_debug
+# from log_conf import LOGGING_CONFIG
