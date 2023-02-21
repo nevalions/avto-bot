a4203ab2a4370e911f95f06c210852dfffe93d14
linroot <linroot@bk.ru>
2023-02-21 23:57:14 +0300

    refactoring

--- src/tg_bot/loader.py
+++ src/tg_bot/loader.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, types, Dispatcher
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import config_tg
+import src.tg_bot.config_tg as config
 
 storage = MemoryStorage()
 
-bot = Bot(token=config_tg.tg_token, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.tg_token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
