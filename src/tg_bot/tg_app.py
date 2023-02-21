18ed9f97af01174a1fff8084020e6b156ae2e667
linroot <linroot@bk.ru>
2023-02-21 23:59:24 +0300

    refactoring

--- src/tg_bot/tg_app.py
+++ src/tg_bot/tg_app.py
@@ -3,7 +3,7 @@ async def on_startup(_dp):
     # from utils.notify_admins import on_startup_notify
     # await on_startup_notify(_dp)
 
-    from tg_bot.utils.set_bot_commands import set_default_commands
+    from src.tg_bot.utils.set_bot_commands import set_default_commands
     await set_default_commands(_dp)
 
     print('Bot started')
