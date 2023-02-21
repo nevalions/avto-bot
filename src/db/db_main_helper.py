641175e0de5ad6393f3a3b5e348de301cd621b4f
linroot <linroot@bk.ru>
2023-02-21 20:05:39 +0300

    refactoring

--- src/db/db_main_helper.py
+++ src/db/db_main_helper.py
@@ -3,9 +3,9 @@ import os
 
 from psycopg2 import sql
 
-from db_main import AutoBotDB as Db
+from src.db.db_main import AutoBotDB as Db
 # sys.path.append(os.path.join(os.getcwd(), '..'))
-import db_queries as queries
+import src.db.db_queries as queries
 
 
 class AutoBotMainDB(Db):
