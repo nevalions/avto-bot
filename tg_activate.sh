221c3cc707857bdf5eab5f159df5cef9bf8573b6
nevalions <linroot@bk.ru>
2023-02-23 11:43:16 +0300

    project refactoring testing

--- tg_activate.sh
+++ tg_activate.sh
@@ -1,4 +1,4 @@
 source /home/www/code/avto-bot/env/bin/activate
 export PYTHONPATH=""
-export PYTHONPATH="${PYTHONPATH}/home/www/code/avto-bot/"
+export PYTHONPATH="${PYTHONPATH}${PWD}"
 python /home/www/code/avto-bot/src/tg_bot/tg_app.py
