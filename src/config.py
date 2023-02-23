4a0f9ba58238757c9afa7c2f3b9b51dc4a52956f
linroot <linroot@bk.ru>
2023-02-23 18:44:56 +0300

    refactoring

--- src/config.py
+++ src/config.py
@@ -1,14 +1,24 @@
 import pathlib
 import os
+import platform
 
 from dotenv import load_dotenv
 
 load_dotenv()
+os_sys = platform.system()
 
 PYTHONPATH = pathlib.Path(str(os.getenv("PYTHONPATH")))
-python_path = pathlib.Path(str(PYTHONPATH).split(':')[0])
-absolut_python_path = python_path.absolute()
-print('PYTHONPATH=' + str(python_path))
+
+if os_sys == 'Windows':
+    print(os_sys)
+    python_path = pathlib.Path(str(PYTHONPATH).split(';')[0])
+    absolut_python_path = python_path.absolute()
+    print('PYTHONPATH=' + str(python_path))
+elif os_sys == 'Linux':
+    print(os_sys)
+    python_path = pathlib.Path(str(PYTHONPATH).split(':')[0])
+    absolut_python_path = python_path.absolute()
+    print('PYTHONPATH=' + str(python_path))
 
 host = str(os.getenv("HOST"))
 user = str(os.getenv("DB_USER"))
