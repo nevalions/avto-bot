35c1afdab79a37be28d1b3a6553fa0424cceeb46
nevalions <linroot@bk.ru>
2023-03-01 20:03:22 +0300

    refactoring maintenance

--- src/tg_bot/handlers/users/allmaintenances.py
+++ src/tg_bot/handlers/users/allmaintenances.py
@@ -1,4 +1,3 @@
-import string
 import logging.config
 
 from aiogram.dispatcher import FSMContext
@@ -15,8 +14,6 @@ from src.tg_bot.keybords.inline import ikb_menu, show_all_car_maintenance_menu, 
 from src.async_db.base import DATABASE_URL, Database
 from src.async_db.cars import CarService
 from src.async_db.maintenances import MaintenanceService
-
-from src.tg_bot.handlers.users import helpers
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
@@ -37,47 +34,33 @@ async def show_cars_maintenances(query: CallbackQuery, callback_data: dict):
     db = Database(DATABASE_URL)
     car_service = CarService(db)
     maintenance_service = MaintenanceService(db)
-    # try:
     car_id = int(callback_data['car_id'])
+    car = await car_service.get_car_by_id(car_id)
     all_maints = await maintenance_service.get_all_car_maintenances(car_id)
+    try:
+        if all_maints:
+            for maint in all_maints:
+                await query.message.answer(
+                    f'Car: {car.model} {car.model_name}\n'
+                    f'Maintenance: {maint["title"]}\n'
+                    f'Date: {maint["date"]}\n'
+                    f'Current mileage: {maint["current_mileage"]}\n',
 
-    for maint in all_maints:
-        car = await car_service.get_car_by_id(car_id)
-        await query.message.answer(
-            f'Car: {car.model} {car.model_name}\n'
-            f'Maintenance: {maint["title"]}\n'
-            f'Date: {maint["date"]}\n'
-            f'Current mileage: {maint["current_mileage"]}\n',
+                    reply_markup=show_all_car_maintenance_menu(maintenance_id=0, car_id=car_id)
+                )
 
-            reply_markup=show_all_car_maintenance_menu(maintenance_id=0, car_id=car_id)
-        )
+            await query.message.answer('Add new maintenance',
+                                       reply_markup=add_new_maintenance(maintenance_id=0, car_id=car_id))
 
-    await query.message.answer('Add new maintenance',
-                               reply_markup=add_new_maintenance(maintenance_id=0, car_id=car_id))
+            await query.message.answer(f'Main menu', reply_markup=ikb_menu)
+        else:
+            autolog_warning(f"Car: {car.model} {car.model_name}\n don't have any maintenances")
+            await query.message.answer(
+                f"Car: {car.model} {car.model_name}\n does not have any maintenances",
+                reply_markup=add_new_maintenance(maintenance_id=0, car_id=car_id))
 
-    await query.message.answer(f'Main menu', reply_markup=ikb_menu)
-
-        #
-        # is_registered = await tg_user_service.get_tg_user_by_chat_id(int(query.message.chat.id))
-        # if is_registered.tg_user_id:
-        #     autolog_info(f"Show all tg_user {query.message.chat.id} cars")
-        #     user_id = is_registered.fk_user
-        #     user_cars = await car_service.get_all_user_cars(user_id)
-        #     if user_cars:
-        #         autolog_info(f"tg_user {query.message.chat.id} have some cars")
-        #         for car in user_cars:
-        #             await query.message.answer(
-        #                 f"ID({car['id']}) {car['model']} {car['model_name']} with current {car['current_mileage']} "
-        #                 f"{car['measures']}\n"
-        #                 f"Info: {car['description']}",
-        #                 reply_markup=show_all_car_maintenance_menu(car['id'])
-        #             )
-        #     else:
-        #         autolog_warning(f"tg_user {query.message.chat.id} don't have any car")
-        #         await query.message.answer("Car don't have any maintenances", reply_markup=ikb_menu)
-        # else:
-        #     await query.message.answer("Car don't have any maintenances", reply_markup=ikb_menu)
-    # except Exception as ex:
-    #     logging.error(ex)
-    # finally:
-    #     await db.engine.dispose()
+            await query.message.answer(f'Main menu', reply_markup=ikb_menu)
+    except Exception as ex:
+        logging.error(ex)
+    finally:
+        await db.engine.dispose()
